e6b9c27ba3579e5b19685bbd5798d2f11fd4e687
Indu <indumathibevara123@gmial.com>
2021-02-09 00:46:17 +0530

    commitby indu

--- java_Parctice/Big_digit.java
+++ java_Parctice/Big_digit.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 		public static void main(String[] args) {
 			
 					Scanner scan = new Scanner(System.in);
-					System.out.println("enter number to find the largest number");
+					System.out.println("enter numbr to find the largest number");
 					int n = scan.nextInt();
 					int[] lst1 = new int[20];
 					
